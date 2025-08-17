441b3bc073cdeeea6fea205b3fe218307e7b8e0e
nihaalxz <nihaal1010@gmail.com>
2025-08-18 05:03:01 +0530

    fixed-google-login-issu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -9,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myfirstflutterapp"
+    namespace = "com.nihaa.p2prental"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -23,24 +22,34 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myfirstflutterapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.nihaa.p2prental"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            storeFile = file("../mykey.jks")
+            storePassword = "android"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug") // or "debug" if you want
         }
     }
 }
+
+
 
 flutter {
     source = "../.."
--- android/app/src/main/kotlin/com/nihaa/p2prental/MainActivity.kt
+++ android/app/src/main/kotlin/com/nihaa/p2prental/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myfirstflutterapp
+package com.nihaa.p2prental
 
 import io.flutter.embedding.android.FlutterActivity
 
